0a8e8fbe54c2499ef3b54cb5b66271d4fe59d163
Alexandru Malgras <alexandru_malgras@yahoo.com>
2023-10-01 20:19:10 +0300

    URLify

---
+++ Arrays and Strings/Arrays and Strings Unit Test/URLifyUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using Arrays_and_Strings.Exercises;
+
+namespace Arrays_and_Strings_Unit_Test
+{
+    public class URLifyUnitTest
+    {
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[]
+            {
+                new char[] { 'M', 'r', ' ', 'J', 'o', 'h', 'n', ' ', 'S', 'm', 'i', 't', 'h', '\0', '\0', '\0', '\0' },
+                new char[] { 'M', 'r', '%', '2', '0', 'J', 'o', 'h', 'n', '%', '2', '0', 'S', 'm', 'i', 't', 'h' }
+            };
+
+            yield return new object[]
+            {
+                new char[] { ' ', 'a', ' ', 'b', ' ', 'c', ' ', 'd', ' ', 'e', ' ', 'f', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' },
+                new char[] { '%', '2', '0', 'a', '%', '2', '0', 'b', '%', '2', '0', 'c', '%', '2', '0', 'd', '%', '2', '0', 'e', '%', '2', '0', 'f' }
+            };
+
+            yield return new object[]
+            {
+                new char[] { 'a', 'b', 'c' },
+                new char[] { 'a', 'b', 'c' }
+            };
+
+            yield return new object[]
+            {
+                new char[] { ' ', '\0', '\0' },
+                new char[] { '%', '2', '0' }
+            };
+
+            yield return new object[]
+            {
+                new char[0],
+                new char[0]
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void URLifyBackwardsTest(char[] input, char[] result)
+        {
+            URLify.URLifyBackwards(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.Equal(input[i], result[i]);
+            }
+        }
+    }
+}
---
+++ Arrays and Strings/Arrays and Strings/Exercises/URLify.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_and_Strings.Exercises
+{
+    /*
+     * Write a method to replace all space in a string with '%20'. You may assume that the string has sufficient
+     * space at the end to hold the additional characters, and that you are given the true length of the string.
+     */
+    public class URLify
+    {
+        /*
+         * One way to solve this problem is to replace the characters from the string to the end by reading the array backwards.
+         */
+        public static void URLifyBackwards(char[] input)
+        {
+            // Check if the array is empty
+            if (input.Length == 0) return;
+
+            int indexToPlace = input.Length - 1;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                if (input[i] == '\0') continue;
+
+                if (input[i] == ' ')
+                {
+                    input[indexToPlace] = '0';
+                    input[indexToPlace - 1] = '2';
+                    input[indexToPlace - 2] = '%';
+
+                    indexToPlace -= 3;
+                }
+                else
+                {
+                    input[indexToPlace] = input[i];
+                    indexToPlace--;
+                }
+            }
+        }
+    }
+}
