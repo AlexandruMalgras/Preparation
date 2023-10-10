14009ef6b553e16bf3c33f47f852cb45923b9404
Alexandru Malgras <alexandru_malgras@yahoo.com>
2023-10-10 06:06:08 +0300

    Partition

---
+++ Linked Lists/Linked Lists Unit Test/PartitionUnitTest.cs
@@ -0,0 +1,67 @@
+﻿using Linked_Lists.Exercises;
+using Linked_Lists.Helpers;
+using System.Text;
+
+namespace Linked_Lists_Unit_Test
+{
+    public class PartitionUnitTest
+    {
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[]
+            {
+                new int[] { 5, 1, 4, 3, 2, 6 },
+                4,
+                "132546"
+            };
+
+            yield return new object[]
+            {
+                new int[] { 5, 1, 4, 3, 2, 6 },
+                7,
+                "514326"
+            };
+
+            yield return new object[]
+            {
+                new int[] { 5, 1, 4, 3, 2, 6 },
+                0,
+                "514326"
+            };
+
+            yield return new object[]
+            {
+                new int[] { },
+                0,
+                ""
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void PartitionWithListsTest(int[] arr, int comparer, string expected)
+        {
+            MyLinkedList<int> linkedList = new MyLinkedList<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                linkedList.AppendToFront(arr[i]);
+            }
+
+            Partition.PartitionWithLists(linkedList, comparer);
+
+            MyNode<int> node = linkedList.Head;
+
+            StringBuilder result = new StringBuilder();
+
+            while (node != null)
+            {
+                result.Append(node.Value.ToString());
+
+                node = node.Next;
+            }
+
+            Assert.Equal(expected, result.ToString());
+        }
+    }
+}
---
+++ Linked Lists/Linked Lists/Exercises/Partition.cs
@@ -0,0 +1,59 @@
+﻿using Linked_Lists.Helpers;
+
+namespace Linked_Lists.Exercises
+{
+    /*
+     * Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes
+     * greater than or equal to x. The partition element x can appear anywhere in the right partition.
+     */
+    public static class Partition
+    {
+        /*
+         * One way to approach this problem is to create 2 sepparate linked lists that will hold the lower and higher values.
+         * Once the values are appended the lists are merged into the main list.
+         */
+        public static void PartitionWithLists(MyLinkedList<int> linkedList, int comparer)
+        {
+            if (linkedList.Count == 0) return;
+
+            MyLinkedList<int> left = new MyLinkedList<int>();
+            MyLinkedList<int> right = new MyLinkedList<int>();
+
+            MyNode<int> node = linkedList.Head;
+
+            while (node != null)
+            {
+                if (node.Value < comparer)
+                {
+                    left.AppendToFront(node);
+                }
+                else
+                {
+                    right.AppendToFront(node);
+                }
+
+                node = node.Next;
+            }
+
+            // Merge linked lists
+            if (left.Tail != null && right.Head != null)
+            {
+                left.Tail.Next = right.Head;
+                right.Head.Previous = left.Tail;
+
+                linkedList.Head = left.Head;
+                linkedList.Tail = right.Tail;
+            }
+            else if (left.Tail != null && right.Head == null)
+            {
+                linkedList.Head = left.Head;
+                linkedList.Tail = left.Tail;
+            }
+            else
+            {
+                linkedList.Head = right.Head;
+                linkedList.Tail = right.Tail;
+            }
+        }
+    }
+}
