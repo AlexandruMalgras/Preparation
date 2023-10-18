3d71a311915207975fe6ecce2ea2cd0b5f1b355d
Alexandru Malgras <alexandru_malgras@yahoo.com>
2023-10-18 22:27:30 +0300

    ListOfDepths

---
+++ Trees and Graphs/Trees and Graphs Unit Test/ListOfDepthsUnitTest.cs
@@ -0,0 +1,89 @@
+﻿using Trees_and_Graphs.Exercises;
+using Trees_and_Graphs.Helpers.Tree;
+
+namespace Trees_and_Graphs_Unit_Test
+{
+    public class ListOfDepthsUnitTest
+    {
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[]
+            {
+                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                4
+            };
+
+            yield return new object[]
+            {
+                new int[] { 1, 2, 3 , 4, 5, 6, 7},
+                3
+            };
+
+            yield return new object[]
+            {
+                new int[] { 1, 2 },
+                2
+            };
+
+            yield return new object[]
+            {
+                new int[] {  },
+                0
+            };
+
+            yield return new object[]
+            {
+                new int[] { 1 },
+                1
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void ListOfDepthsWithBFSTest(int[] values, int count)
+        {
+            Tree<int> tree = new Tree<int>();
+
+            MinimalTree.CreateBinarySearchTree(tree, values);
+
+            List<LinkedList<TreeNode<int>>> linkedLists = ListOfDepths.ListOfDepthsWithBFS(tree);
+
+            int countLinkedLists;
+
+            if (linkedLists != null)
+            {
+                countLinkedLists = linkedLists.Count;
+            }
+            else
+            {
+                countLinkedLists = 0;
+            }
+
+            Assert.Equal(count, countLinkedLists);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void ListOfDepthsWithDFSTest(int[] values, int count)
+        {
+            Tree<int> tree = new Tree<int>();
+
+            MinimalTree.CreateBinarySearchTree(tree, values);
+
+            List<LinkedList<TreeNode<int>>> linkedLists = ListOfDepths.ListOfDepthsWithDFS(tree);
+
+            int countLinkedLists;
+
+            if (linkedLists != null)
+            {
+                countLinkedLists = linkedLists.Count;
+            }
+            else
+            {
+                countLinkedLists = 0;
+            }
+
+            Assert.Equal(count, countLinkedLists);
+        }
+    }
+}
---
+++ Trees and Graphs/Trees and Graphs/Exercises/ListOfDepths.cs
@@ -0,0 +1,74 @@
+﻿using Trees_and_Graphs.Helpers.Tree;
+
+namespace Trees_and_Graphs.Exercises
+{
+    /*
+     * Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth
+     * (e.g., if you have a tree with depth D, you'll have D linked lists).
+     */
+    public static class ListOfDepths
+    {
+        public static List<LinkedList<TreeNode<T>>> ListOfDepthsWithBFS<T>(Tree<T> tree)
+        {
+            if (tree.root == null || tree == null) return null;
+
+            List<LinkedList<TreeNode<T>>> linkedLists = new List<LinkedList<TreeNode<T>>>();
+
+            LinkedList<TreeNode<T>> current = new LinkedList<TreeNode<T>>();
+
+            current.AddLast(tree.root);
+
+            // Loop as long as there are children
+            while (current.Count > 0)
+            {
+                linkedLists.Add(current);
+
+                // Move to the next depth
+                LinkedList<TreeNode<T>> parents = current;
+
+                // Empty the current list
+                current = new LinkedList<TreeNode<T>>();
+
+                // Add the children to the current list
+                foreach (TreeNode<T> node in parents)
+                {
+                    if (node.Left != null) current.AddLast(node.Left);
+                    if (node.Right != null) current.AddLast(node.Right);
+                }
+            }
+
+            return linkedLists;
+        }
+
+
+        public static List<LinkedList<TreeNode<T>>> ListOfDepthsWithDFS<T>(Tree<T> tree)
+        {
+            if (tree.root == null || tree == null) return null;
+
+            List<LinkedList<TreeNode<T>>> linkedLists = new List<LinkedList<TreeNode<T>>>();
+
+            ListOfDepthsWithDFS(linkedLists, tree.root, 0);
+
+            return linkedLists;
+        }
+
+        private static void ListOfDepthsWithDFS<T>(List<LinkedList<TreeNode<T>>> linkedLists, TreeNode<T> node, int depth)
+        {
+            // Base case
+            if (node == null) return;
+
+            // Create the linked list if it does not already exist
+            if (linkedLists.Count == depth)
+            {
+                linkedLists.Add(new LinkedList<TreeNode<T>>());
+            }
+
+            // Assign the node to the linked list
+            linkedLists[depth].AddLast(node);
+
+            // Recur for the rest of the nodes
+            ListOfDepthsWithDFS(linkedLists, node.Left, depth + 1);
+            ListOfDepthsWithDFS(linkedLists, node.Right, depth + 1);
+        }
+    }
+}
